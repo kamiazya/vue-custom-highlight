94d267cdffa357707095a7c4644ce71534150281
kamiazya <yuki@kamiazya.tech>
2024-06-21 00:26:56 +0900

    feat: Improve example app

--- example/src/App.vue
+++ example/src/App.vue
@@ -2,23 +2,19 @@
 import { vCustomHighlight } from "@kamiazya/vue-custom-highlight";
 import { ref } from "vue";
 
-const keyword = ref("a");
+import sample from "./assets/sample.txt?raw";
+const keyword = ref<string>('e');
+
+const text = ref(sample);
 </script>
 
 <template>
   <input v-model="keyword" type="search" />
-  <div v-custom-highlight:custom="keyword">
-    <p>ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
-    <p>abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz</p>
-    <p>01234567890123456789</p>
-    <p>あいうえおかきくけこさしすせそたちつてとなにぬねの</p>
-    <p>アイウエオカキクケコサシスセソタチツテトナニヌネノ</p>
-    <p>亜伊宇江於加木久介子糸須世曽多知都天止奈仁奴禰乃</p>
-  </div>
+  <div v-custom-highlight="keyword">{{ text }}</div>
 </template>
 
-<style scoped>
-::highlight(custom) {
+<style>
+::highlight(v-custom-highlight) {
   background-color: yellow;
 }
 </style>
