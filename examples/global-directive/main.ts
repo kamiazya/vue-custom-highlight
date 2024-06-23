c6a25d09b1d62405987a57caaf1770985040e514
kamiazya <yuki@kamiazya.tech>
2024-06-23 17:47:42 +0900

    chore: Add comment for vHighlight directive registoration

--- examples/global-directive/main.ts
+++ examples/global-directive/main.ts
@@ -1,9 +1,9 @@
-import { vHighlight } from '@kamiazya/vue-highlight'; // Import the 'Keyword' type
+import { vHighlight } from '@kamiazya/vue-highlight'; // Import the directive
 import { createApp } from 'vue';
 
 import App from './src/App.vue';
 
 const app = createApp(App);
 // @ts-ignore
-app.directive('highlight', vHighlight);
+app.directive('highlight', vHighlight); // Register the directive
 app.mount('#container');
